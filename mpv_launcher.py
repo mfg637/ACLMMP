70ca14714b3c0ad0e3728b3ad60623bb11ace517
mfg637 <mfg636@gmail.com>
2021-11-12 16:51:50 +0200

    ACLMMP mpv launcher: using pathlib to relativise references paths

--- mpv_launcher.py
+++ mpv_launcher.py
@@ -3,15 +3,19 @@
 
 import argparse
 import subprocess
+import pathlib
 
 if __name__ == "__main__":
     import srs_parser
 else:
     from . import srs_parser
 
-def launch_mpv(srs_file, level, limit_audio_channels=None):
-    content_metadata, streams_metadata, minimal_content_compatibility_level = srs_parser.parseJSON(srs_file)
-    srs_file.close()
+
+def launch_mpv(srs_file: pathlib.Path, level, limit_audio_channels=None):
+    dir = srs_file.parent
+    f = srs_file.open("r")
+    content_metadata, streams_metadata, minimal_content_compatibility_level = srs_parser.parseJSON(f)
+    f.close()
 
     if content_metadata['media-type'] != srs_parser.MEDIA_TYPE.VIDEO.value:
         raise NotImplementedError(
@@ -30,7 +34,7 @@ def launch_mpv(srs_file, level, limit_audio_channels=None):
     commandline = ["mpv", "--title={}".format(CONTENT_TITLE)]
 
     if streams_metadata[0] is not None:
-        commandline += [streams_metadata[0].get_compatible_files(level)[0]]
+        commandline += [dir.joinpath(streams_metadata[0].get_compatible_files(level)[0])]
 
     if streams_metadata[1] is not None:
         line = "--audio-files="
@@ -38,17 +42,17 @@ def launch_mpv(srs_file, level, limit_audio_channels=None):
             if limit_audio_channels is not None:
                 file = stream.get_file(limit_audio_channels, level)
                 if file is not None:
-                    line += "{}:".format(file)
+                    line += "{}:".format(dir.joinpath(file))
                     continue
             files = stream.get_compatible_files(level)
             for file in files:
-                line += "{}:".format(file)
+                line += "{}:".format(dir.joinpath(file))
         commandline += [line]
 
     if streams_metadata[2] is not None:
         line = "--sub-files="
         for stream in streams_metadata[2]:
-            line += "{}:".format(stream.get_compatible_files(level)[0])
+            line += "{}:".format(dir.joinpath(stream.get_compatible_files(level)[0]))
         commandline.append(line)
 
     print("COMMANDLINE", commandline)
@@ -57,7 +61,7 @@ def launch_mpv(srs_file, level, limit_audio_channels=None):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("SRS_file", help="Stream References Sheet file in JSON format (*.srs.json)", type=open)
+    parser.add_argument("SRS_file", help="Stream References Sheet file in JSON format (*.srs.json)", type=pathlib.Path)
     parser.add_argument("--level", help="Target content compatibility level", required=True, type=int)
     parser.add_argument("--limit_audio_channels", type=int)
     args = parser.parse_args()
